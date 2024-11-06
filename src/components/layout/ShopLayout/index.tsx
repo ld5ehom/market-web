d1ceec9a45e517182dd0709adba1567c8d891715
Taewook Park <155722702+ld5ehom@users.noreply.github.com>
2024-11-06 12:54:16 -0800

    Footer update
    Footer UI Update

--- src/components/layout/ShopLayout/index.tsx
+++ src/components/layout/ShopLayout/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 
+import Footer from './_components/Footer'
 import Header from './_components/Header'
 import UserInfo from './_components/UserInfo'
 
@@ -12,7 +13,14 @@ export default function ShopLayout({ children }: Props) {
         <div style={{ minWidth: 1000 }}>
             <UserInfo />
             <Header />
-            {children}
+            <main
+                style={{
+                    minHeight: 'calc(100vh - 28px - 108px - 65px)',
+                }}
+            >
+                {children}
+            </main>
+            <Footer />
         </div>
     )
 }
